ffa9d3b814b3bc3cddd307cebc44051375b77915
Glowerwur <kubikkohoutek88@gmail.com>
2024-11-02 21:10:26 +0100

    test

--- src/main/java/cz/tourdeapp/springtemplate/HelloController.java
+++ src/main/java/cz/tourdeapp/springtemplate/HelloController.java
@@ -1,15 +1,14 @@
 package cz.tourdeapp.springtemplate;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
+@Controller  // Change @RestController to @Controller to serve an HTML page
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello world!";
+        // Return the name of the HTML template (without the .html extension)
+        return "index";
     }
-
 }
